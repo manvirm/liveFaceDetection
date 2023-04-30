96de004f987fd25b365f58c5cf447af9d20e8cb3
Manvir Mann <manvirm29@gmail.com>
2023-04-29 22:34:24 -0700

    Set dimensions and capacities

---
+++ main.py
@@ -0,0 +1,10 @@
+import threading
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
